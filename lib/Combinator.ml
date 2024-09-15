7f0aa54caa81e15221d6930a0b79c83f097efb6d
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2024-09-15 16:42:10 +0800

    misc
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- lib/Combinator.ml
+++ lib/Combinator.ml
@@ -8,11 +8,20 @@ type _ Effect.t += Next : Lexer.token Asai.Range.located Effect.t
 let next_token ()  = perform Next
 let shift pos = perform (Shift pos)
 let current_position () : Lexing.position = perform CurrentPosition
-let peek_token ()  = 
-  let pos = current_position () in
-  let tok = next_token () in
-  shift pos;
-  tok
+
+let run (filename : string) (lexbuf : Lexing.lexbuf) (f : unit -> 'a) : 'a = 
+  try_with f ()
+  { effc = fun (type a) (eff: a t) ->
+    match eff with
+    | Next -> Some (fun (k: (a,_) continuation) -> 
+      let tok = Lexer.token lexbuf in
+      let loc = Asai.Range.of_lexbuf ~source:(`File filename) lexbuf in
+      let tok = (Asai.Range.locate loc tok) in
+      continue k tok)
+    | Shift pos -> Some (fun k -> Lexing.set_position lexbuf pos; continue k ())
+    | CurrentPosition -> Some (fun k -> continue k lexbuf.lex_curr_p)
+    | _ -> None
+  }
 
 let consume (predict : Lexer.token) : unit =
   let tok = next_token () in
@@ -23,20 +32,6 @@ let consume (predict : Lexer.token) : unit =
     Reporter.fatalf ~loc Parse_error "expected `%s`, but got `%s`"
       ([%show: Lexer.token] predict)
       ([%show: Lexer.token] tok.value)
-
-let run (filename : string) (lexbuf : Lexing.lexbuf) (f : unit -> 'a) : 'a = 
-  try_with f ()
-  { effc = fun (type a) (eff: a t) ->
-    match eff with
-    | Next -> Some (fun (k: (a,_) continuation) -> 
-      let tok = Lexer.token lexbuf in
-      let loc = Asai.Range.of_lexbuf ~source:(`File filename) lexbuf in
-      let tok = (Asai.Range.locate loc tok)in
-      continue k tok)
-    | Shift pos -> Some (fun k -> Lexing.set_position lexbuf pos; continue k ())
-    | CurrentPosition -> Some (fun k -> continue k lexbuf.lex_curr_p)
-    | _ -> None
-  }
 
 let many (p : unit -> 'a) : 'a list =
   let res = ref [] in
@@ -52,6 +47,5 @@ let many (p : unit -> 'a) : 'a list =
 let (<|>) (p1 : unit -> 'a) (p2 : unit -> 'a) () : 'a =
   let pos = current_position () in
   let x = (try p1 () with | _ ->
-    shift pos; p2 ()
-      ) in
+    shift pos; p2 ()) in
   x
