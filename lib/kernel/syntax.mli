4e599cbec75518231eafac329b7aa9a9e8e54c0d
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2026-05-27 23:38:55 +0800

    give `mli` files

---
+++ lib/kernel/syntax.mli
@@ -0,0 +1,120 @@
+type binder_name =
+  | Named of string
+  | Anon
+[@@deriving show]
+
+module Name : sig
+  type t = binder_name =
+    | Named of string
+    | Anon
+
+  val to_string : t -> string
+end
+
+type 't binder =
+  { name : binder_name
+  ; bound : 't
+  ; implicit : bool
+  }
+[@@deriving show]
+
+module Core : sig
+  type metavar = MetaVar of int [@@deriving show]
+
+  type term =
+    | Universe of Level.level
+    | LocalVar of int
+    | Var of string
+    | App of term * term
+    | Lambda of term binder
+    | TypedLambda of typ binder * term
+    | Pi of typ binder * typ
+    | Meta of metavar
+    | InsertedMeta of metavar * int
+    | Lift of
+        { from_lvl : Level.level
+        ; to_lvl : Level.level
+        ; ty : term
+        }
+    | LiftTerm of
+        { from_lvl : Level.level
+        ; to_lvl : Level.level
+        ; ty : term
+        ; tm : term
+        }
+    | UnliftTerm of
+        { from_lvl : Level.level
+        ; to_lvl : Level.level
+        ; ty : term
+        ; tm : term
+        }
+    | RecordType of
+        { name : string
+        ; params : term list
+        ; fields : typ binder list
+        }
+    | RecordIntro of
+        { name : string
+        ; fields : (string * term) list
+        }
+    | RecordProj of
+        { record : term
+        ; field : string
+        }
+    | IdAbsurd of term
+  [@@deriving show]
+
+  and typ = term [@@deriving show]
+
+  type value =
+    | Flex of metavar * value Bwd.bwd
+    | RigidLocal of int * value Bwd.bwd
+    | Var of string * value Bwd.bwd
+    | IndType of string * value Bwd.bwd
+    | Label of string * value Bwd.bwd
+    | Elim of elim_head * value Bwd.bwd
+    | VLambda of (value -> value) binder
+    | VPi of value_ty binder * (value -> value)
+    | Universe of Level.level
+    | VLift of
+        { from_lvl : Level.level
+        ; to_lvl : Level.level
+        ; ty : value
+        }
+    | VLiftTerm of
+        { from_lvl : Level.level
+        ; to_lvl : Level.level
+        ; ty : value
+        ; tm : value
+        }
+    | VUnliftTerm of
+        { from_lvl : Level.level
+        ; to_lvl : Level.level
+        ; ty : value
+        ; tm : value
+        }
+    | VRecordType of
+        { name : string
+        ; params : value list
+        ; fields : value_ty binder list
+        ; field_env : value Bwd.bwd
+        ; field_terms : typ binder list
+        }
+    | VRecordIntro of
+        { name : string
+        ; fields : (string * value) list
+        }
+    | VRecordProj of value * string
+    | VIdAbsurd of value
+  [@@deriving show]
+
+  and elim_head =
+    { elim_name : string
+    ; reducer : value Bwd.bwd -> value option
+    }
+
+  and value_ty = value
+
+  val rigid_local : int -> value
+  val lvl_to_ix : env_size:int -> int -> int
+end
---
+++ lib/surface/surface.mli
@@ -0,0 +1,154 @@
+type binder_name = Violet_kernel.Syntax.binder_name =
+  | Named of string
+  | Anon
+[@@deriving show]
+
+type 't binder = 't Violet_kernel.Syntax.binder =
+  { name : binder_name
+  ; bound : 't
+  ; implicit : bool
+  }
+[@@deriving show]
+
+module Name = Violet_kernel.Syntax.Name
+
+type preterm =
+  | Located of preterm Asai.Range.located
+  | Universe
+  | Hole
+  | Goal of string option
+  | Var of string list
+  | App of bool * preterm * preterm
+  | Lambda of preterm binder
+  | TypedLambda of pretype binder * preterm
+  | Pi of pretype binder * pretype
+  | Max of preterm * preterm
+  | Op_soup of soup_item list
+  | RecordLit of (string * preterm) list
+  | RecordUpdate of preterm * (string * preterm) list
+  | Proj of preterm * string
+  | IdAbsurd of preterm
+  | Inline_elim of inline_elim_data
+[@@deriving show]
+
+and inline_elim_data =
+  { target : string
+  ; siblings : (clause * pattern list) list
+  ; outer_subst : (int * Violet_kernel.Syntax.Core.value) list
+  ; target_override : preterm option
+  }
+
+and soup_item =
+  | SI_Atom of preterm
+  | SI_Name of string * Asai.Range.t option
+  | SI_Imp_arg of preterm
+[@@deriving show]
+
+and pretype = preterm
+
+and pattern =
+  | PVar of string
+  | PWildcard
+  | PCon of string * pattern list
+  | PImpVar of string
+  | PRecord of (string * pattern) list
+
+and clause =
+  { head : string
+  ; patterns : pattern list
+  ; body : preterm
+  }
+[@@deriving show]
+
+type as_arg =
+  { term : preterm
+  ; implicit : bool
+  }
+
+type stack_move =
+  | Intro
+  | Split
+[@@deriving show]
+
+type op_name_path = string list [@@deriving show]
+
+type op_assoc =
+  | OA_Left
+  | OA_Right
+  | OA_None
+[@@deriving show]
+
+type op_option =
+  | OO_Weaker_than of op_name_path list
+  | OO_Stronger_than of op_name_path list
+  | OO_Same_as of op_name_path list
+  | OO_Associativity of op_assoc
+[@@deriving show]
+
+type top =
+  | Let of
+      { name : string
+      ; name_loc : Asai.Range.t option
+      ; bindings : pretype binder list
+      ; result_ty : pretype
+      ; body : preterm
+      }
+  | Data of
+      { name : string
+      ; name_loc : Asai.Range.t option
+      ; params : pretype binder list
+      ; deps : pretype binder list
+      ; ind_ty : pretype
+      ; ind_ty_loc : Asai.Range.t option
+      ; ctors : pretype binder list
+      ; ctor_name_locs : Asai.Range.t option list
+      }
+  | Universe_decl of (string * Asai.Range.t option) list
+  | Stack_def of
+      { name : string
+      ; name_loc : Asai.Range.t option
+      ; params : pretype binder list
+      ; signature : pretype
+      ; moves : stack_move list
+      ; clauses : clause list
+      }
+  | Elim_def of
+      { name : string
+      ; name_loc : Asai.Range.t option
+      ; params : pretype binder list
+      ; signature : pretype
+      ; opens : string list
+      ; intros : (string * bool) list
+      ; target : string
+      ; clauses : clause list
+      }
+  | Operator_decl of
+      { template : string
+      ; body : preterm
+      ; options : op_option list
+      }
+  | Record of
+      { name : string
+      ; name_loc : Asai.Range.t option
+      ; params : pretype binder list
+      ; ind_ty : pretype
+      ; fields : pretype binder list
+      }
+[@@deriving show]
+
+type t =
+  { name : string
+  ; imports : Yuujinchou.Trie.path list
+  ; exports : (string * Asai.Range.t option) list
+  ; tops : top Asai.Range.located list
+  }
+
+val lambda : string list -> preterm -> preterm
+val typed_lambda : pretype binder list -> preterm -> preterm
+val telescope : pretype -> pretype binder list
+val codomain : pretype -> pretype
+val codomain_loc : pretype -> Asai.Range.t option
+val pi : pretype binder list -> pretype -> pretype
+val applied_spine : preterm -> preterm list
+val apply : preterm -> preterm list -> preterm
+val apply_tele : preterm -> preterm binder list -> preterm
